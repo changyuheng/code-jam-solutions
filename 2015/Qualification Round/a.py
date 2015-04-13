639651a3e947224d15d94b3d661be96f83e16e45
Chang Yu-heng <mr.changyuheng@gmail.com>
2015-04-13 22:04:58 +0800

    Problem A Qualification Round 2015

---
+++ 2015/Qualification Round/a.py
@@ -0,0 +1,15 @@
+T = int(input())
+for ti in range(1, T + 1):
+    l = input().split()
+    smax = l[0]
+    audience = l[1]
+    n = 0
+    ups = 0
+
+    for i, p in enumerate(audience):
+        if ups < i:
+            n += i - ups
+            ups = i
+        ups += int(p)
+
+    print('Case #{}: {}'.format(ti, n))
